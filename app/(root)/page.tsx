f8a2109e8c6e2f98019606b737df8fc15396501c
Himanshu wadhwa <himanshuhw999@gmail.com>
2025-02-06 14:08:05 +0530

    project Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
+import { ProjectCard } from "@/components/ProjectCard";
 import SearchForm from "../../components/searchForm";
 interface iQueryprops{
   searchParams: Promise<{ search?:string }>
 }
 
 export default async function Home({searchParams} : iQueryprops ) {
-  const search = (await searchParams).search
+  const search = (await searchParams).search;
+  const posts = [{
+    createdAt : new Date(),
+    views : "10",
+    author : {_id : 1, name:'Himanshu'},
+    _id :1,
+    description : "Description",
+    image : "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg",
+    category : "Animals",
+    title : "Animal Lives"
+  }]
   return (
     <>
     <section className="pink_container">
@@ -13,6 +24,19 @@ export default async function Home({searchParams} : iQueryprops ) {
       <SearchForm search={search}/>
     </section>
     
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {search ? `Search results for "${search}" ` : 'All Projects'}
+      </p>
+      <ul className="mt-7 card_grid">
+        {posts.length >0 ? (
+          posts.map((post : StartupCardType) =>(
+          <ProjectCard key={post?._id} post={post}/>
+        ))
+        ) : ( <p className="no_result">No results</p>
+        )}
+      </ul>
+    </section>
     </>
   );
 }
